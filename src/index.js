312506a33f7cf8b9b6c34d7ac7082b05f4f246f6
Jeffrey Lowy <jeffreylowy@gmail.com>
2017-12-31 01:17:30 -0800

    Ignore public, add React, Router, React-DOM

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
 import './less/main.less';
+import React from 'react'
+import { BrowserRouter as Router,Route,Link } from 'react-router-dom'
 import anime from 'animejs';
-
-let item1 = anime({
-	targets: '.item-1',
-	left:"250px"
-  });
-
-  let item2 = anime({
-	targets: '.item-2',
-	left: "0px"
-  });  
