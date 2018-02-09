c7bf98b2b57463dfd1144aced270891c96854eab
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-02-09 08:29:16 -0800

    Fix eslint errors in Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 // Require the path and ExtractTextPlugin modules. 
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 // Create a new object from ExtractTextPlugin. Set the filename to style.css and save in the public/css directory. The path is based on the output directory in the object referenced by module.exports
 
 const extractLess = new ExtractTextPlugin({
 	// Here is a way to name your files with a hash - filename: "../css/[name].[contenthash].css"
-	filename: "css/style.css",
-	disable: process.env.NODE_ENV === "development"
+	filename: 'css/style.css',
+	disable: process.env.NODE_ENV === 'development'
 });
 
 const uglifyJs = new UglifyJsPlugin({
 	test: /\.js($|\?)/i,
 	sourceMap: true,
 	extractComments: true
-  })
+});
 
 module.exports = {
 	entry: {
@@ -29,29 +29,29 @@ module.exports = {
 		rules: [{
 			test: /\.js$/,
 			exclude: /node_modules/,
-			loader: "babel-loader"
+			loader: 'babel-loader'
 		}, {
 			test: /\.less$/,
 			use: extractLess.extract({
 				use: [{
-					loader: "css-loader",
+					loader: 'css-loader',
 					options: {
 						sourceMap: true
 					}
 				}, {
-					loader: "less-loader",
+					loader: 'less-loader',
 					options: {
 						sourceMap: true
 					}
 				}],
 				// use style-loader in development
-				fallback: "style-loader"
+				fallback: 'style-loader'
 			})
 		}, {
 			test: /\.(ttf|woff|woff2)$/,
-			loader: "file-loader",
+			loader: 'file-loader',
 			options: {
-				publicPath: "../",
+				publicPath: '../',
 				outputPath: 'fonts/',
 				name: '[name].[ext]'
 			}
