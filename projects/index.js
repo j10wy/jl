ac07ab482eec37eff86ddad7c65e80f52754bbd9
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-02-11 19:35:25 -0800

    Add proj route with liri project

---
+++ projects/index.js
@@ -0,0 +1,5 @@
+const liri = require('./liri');
+
+module.exports = {
+	liri: liri
+};
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
-var template = require('../templates/main');
+const template = require('../templates/main');
+const projects = require('../projects');
 
 router.get('/', function (req, res) {
 	res.marko(template, {
 		name: 'Jeff',
 		colors: ['red', 'green', 'blue']
 	});
+});
+
+router.get('/portfolio/:proj', function (req, res) {
+	var proj = req.params.proj;
+	res.marko(projects[proj]);
 });
 
 module.exports = router;
