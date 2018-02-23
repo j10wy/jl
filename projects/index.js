12877226f676e72025ed905af84343173a37ee7c
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-02-23 00:38:48 -0800

    Project templates

--- projects/index.js
+++ projects/index.js
@@ -1,5 +1,13 @@
+const giphy = require('./guardians-of-the-galaxy');
+const gotg = require('./guardians-of-the-galaxy');
+const got_rpg = require('./game-of-thrones');
+const lead_generator = require('./lead-generator');
 const liri = require('./liri');
 
 module.exports = {
-	liri: liri
+	liri: liri,
+	"giphy-api": giphy,
+	"game-of-thrones": got_rpg,
+	"guardians-of-the-galaxy": gotg,
+	"lead-generator": lead_generator
 };
