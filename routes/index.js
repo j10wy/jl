7dd993aa5c37ac24930f4fdfc6617e35d3eb4fe5
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-02-28 15:18:34 -0800

    [WIP] About Me section

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 const router = require('express').Router();
 const template = require('../templates/home');
+const about = require('../templates/about');
 const projects = require('../projects');
 
 router.get('/', function (req, res) {
 	res.marko(template, {
 		title: 'Jeff Lowy'
+	});
+});
+
+router.get('/about', function (req, res) {
+	res.marko(about, {
+		title: 'About Me'
 	});
 });
 
