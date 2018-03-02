c5abafcffa618ba65ec606c8a16e63c74acdba5e
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-03-02 12:10:31 -0800

    Remove header info per Zapier docs

--- src/js/form_submit.js
+++ src/js/form_submit.js
@@ -20,14 +20,8 @@ function submitForm(data) {
 	// Default options are marked with *
 	return fetch('https://hooks.zapier.com/hooks/catch/702577/koyv66/', {
 			body: JSON.stringify(data), // must match 'Content-Type' header
-			cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-			headers: {
-				'content-type': 'application/json'
-			},
 			method: 'POST',
 			mode: 'cors', // no-cors, *same-origin
-			redirect: 'follow', 
-			referrer: 'no-referrer'
 		})
 		.then(response => response.json()) // parses response to JSON
 }
