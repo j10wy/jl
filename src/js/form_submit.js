e35f12b1382084c6def0fb1cb045964d260d21f4
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-03-02 12:38:01 -0800

    Store form elements in variables and clear message box.

--- src/js/form_submit.js
+++ src/js/form_submit.js
@@ -1,27 +1,36 @@
-var formSubmitButton = document.getElementById("form-submit");
+if (window.location.pathname.indexOf("/contact") === 0) {
+	var formSubmitButton = document.getElementById("form-submit");
 
-formSubmitButton.addEventListener("click", (event) => {
+	formSubmitButton.addEventListener("click", (event) => {
 
-	event.preventDefault();
-	let data = {
-		name: document.getElementById("name").value,
-		email: document.getElementById("email").value,
-		message: document.getElementById("message").value
-	};
-	
-	submitForm(data)
-		.then(response => console.log(response))
-		.catch(error => console.error(error));
+		let name_field = document.getElementById("name"),
+			email_field = document.getElementById("email"),
+			message_box = document.getElementById("message");
 
-});
+		event.preventDefault();
+		let data = {
+			name: name_field.value,
+			email: email_field.value,
+			message: message_box.value
+		};
 
-function submitForm(data) {
-	console.log(data);
-	// Default options are marked with *
-	return fetch('https://hooks.zapier.com/hooks/catch/702577/koyv66/', {
-			body: JSON.stringify(data), // must match 'Content-Type' header
-			method: 'POST',
-			mode: 'cors', // no-cors, *same-origin
-		})
-		.then(response => response.json()) // parses response to JSON
+		submitForm(data)
+			.then((response) => {
+				console.log("Message delivery status: ", response.status);
+				message_box.value = "";
+			})
+			.catch(error => console.error(error));
+
+	});
+
+	function submitForm(data) {
+		console.log(data);
+		// Default options are marked with *
+		return fetch('https://hooks.zapier.com/hooks/catch/702577/koyv66/', {
+				body: JSON.stringify(data),
+				method: 'POST',
+				mode: 'cors',
+			})
+			.then(response => response.json()) // parses response to JSON
+	}
 }
